a388491f9881a8829a049d3a0470b5c8d7dd0be3
hfq <15737129091@163.com>
2020-03-16 14:17:07 +0800

    登录验证

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,27 @@
 /*
 包含应用中所有请求接口的函数：接口请求函数
 */
-
 import ajax from './ajax';
 
 const BASE = ''
 
-export function reqLogin(username, password) {
-    ajax({
-        method: 'post',
-        url: BASE + '/login',
-        data: {
-            username,
-            password
-        }
-    })
-}
+export const reqLogin = (username, password) => ajax.post(BASE + '/login',{username, password})
+//以上为简写
+// ajax({
+//     method: 'post',
+//     url: BASE + '/login',
+//     data: { //data对象，默认使用json格式的请求体携带参数数据
+//         username,
+//         password
+//     }
+// })
 
-const name = 'admin'
-const pwd = 'admin'
-reqLogin(name, pwd)
+
+// const name = 'admin'
+// const pwd = 'admin'
+// reqLogin(name, pwd).then(result => {
+//     // const result = response.data
+//     console.log('请求成功了', result)
+// }, error => {
+//     console.log('请求失败了', error)
+// })
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,84 +1,87 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import React, {Component} from 'react';
+import { Form,Icon, Input, Button, Checkbox, message } from 'antd';
+import { reqLogin } from '../../api/index';
 import logo from '../../static/images/logo.png';
 import './login.less';
 
 
-const NormalLoginForm = () => {
-	const onFinish = values => {
-	  console.log('Received values of form: ', values);
-	
+class Login extends Component {
+	handleSubmit = e => {
+		e.preventDefault()
+		this.props.form.validateFields(async(err, {username,password}) => {
+			// if(!err) {
+			// 	const result = await reqLogin(username,password)
+			// 	if(result.status===0) {
+			//	    message.success('登录成功')
+			// 		this.props.history.replace('/')
+			// 	} else {
+			// 		message.error(result.msg)
+			// 	}
+			// } else {
+			// 	alert('验证失败')
+			// }
+			if(!err) {
+				if (username === 'admin' && password  === 'admin') {
+					message.success('登录成功')
+					this.props.history.replace('/')
+				}
+			} else {
+				message.error("验证失败")
+			}
+		})
 	}
-	const onFinishFailed = errorInfo => {
-		alert('验证失败！')
-	}
-
-	return (
-		<div className="login">
-			<div className="login-box">
-				<div className="login-logo">
-					<img src={logo} alt="logo" />
-				</div>
-				<Form
-					name="normal_login"
-					className="login-form"
-					initialValues={{ remember: true }}
-					onFinish={onFinish}
-					onFinishFailed={onFinishFailed}
-				>
-					<Form.Item
-						name="username"
-						rules={[
-							{ required: true, message: '请输入用户名！' },
-							{ min: 4, message: '用户名不能小于4位！' },
-							{ max: 12, message: '用户名不能大于12位！' },
-							{ pattern: /[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成！' }
-						]}
+	render() { 
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div className="login">
+				<div className="login-box">
+					<div className="login-logo">
+						<img src={logo} alt="logo" />
+					</div>
+					<Form
+						onSubmit= {this.handleSubmit}
+						className="login-form"
 					>
-						<Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
-					</Form.Item>
-					<Form.Item
-						name="password"
-						rules={[
-							({ getFieldValue }) => ({
-								validator(rule, value) {
-									if(!value) {
-										return Promise.reject('请输入密码!')
-									} else if(value.length<4) {
-										return Promise.reject('密码不能小于4位！')
-									} else if(value.length>12) {
-										return Promise.reject('密码不能大于12位！')
-									} else if(!/[a-zA-Z0-9_]+$/.test(value)) {
-										return Promise.reject('密码必须是英文、数字或下划线组成！')
-									} else {
-										return Promise.resolve();
-									}
-								}
-							})
-						]}
-					>
-						<Input
-							prefix={<LockOutlined className="site-form-item-icon" />}
-							type="password"
-							placeholder="密码"
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Form.Item name="remember" valuePropName="checked" noStyle>
-							<Checkbox style={{color:'#fff'}}>记住密码</Checkbox>
+						<Form.Item>
+							{getFieldDecorator('username',{
+								rules:[
+									{ required: true, message: '请输入用户名！' },
+									{ min: 4, message: '用户名不能小于4位！' },
+									{ max: 12, message: '用户名不能大于12位！' },
+									{ pattern: /[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成！' }
+								]
+							})(
+								<Input prefix={<Icon type="user" />} placeholder="用户名" />
+							)}
 						</Form.Item>
-					</Form.Item>
-
-					<Form.Item>
-						<Button type="primary" htmlType="submit" className="login-form-button">
-							登录
-						</Button>
-					</Form.Item>
-				</Form>
+						<Form.Item>
+							{getFieldDecorator('password',{
+								rules:[
+									{ required: true, message: '请输入密码！' },
+									{ min: 4, message: '密码不能小于4位！' },
+									{ max: 12, message: '密码不能大于12位！' },
+									{ pattern: /[a-zA-Z0-9_]+$/, message: '密码必须是英文、数字或下划线组成！' }
+								]
+							})(
+								<Input
+									prefix={<Icon type="lock" />}
+									type="password"
+									placeholder="密码"
+								/>
+							)}
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit" className="login-form-button">
+								登录
+							</Button>
+						</Form.Item>
+					</Form>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
-export default NormalLoginForm;
+const WrapperForm = Form.create()(Login)
+ 
+export default WrapperForm;
